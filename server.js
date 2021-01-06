b766fc0f8520bbb6d6a92e2ad88812a98288ca63
minhld99 <50240885+minhld99@users.noreply.github.com>
2021-01-06 21:59:32 +0700

    Complete basic function, downgrade module version

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+connect().use(serveStatic("./src")).listen(8080, function(){
+    console.log('Server running on 8080...');
+    console.log('Open in browser: http://localhost:8080/');
+});

71fb91c5ab845e420621a115205526e3cfaec837
minhld99 <50240885+minhld99@users.noreply.github.com>
2021-01-09 15:44:02 +0700

    Move index.html to outer scope

--- local_server.js
+++ local_server.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic("./src")).listen(8080, function(){
+connect().use(serveStatic("./")).listen(8080, function(){
     console.log('Server running on 8080...');
     console.log('Open in browser: http://localhost:8080/');
 });
